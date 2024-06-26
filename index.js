86e88936af62a362d5a102126abe068b76e4f8bd
gusty <gustavodiorlando@gmail.com>
2024-06-26 13:36:46 -0300

    Add error page and app.gets instead of static

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('pages'));
+app.get('/', (req, res) => {
+  const file = path.join(__dirname, 'pages', 'index.html');
+  res.sendFile(file);
+})
+
+app.get('/about', (req, res) => {
+  const file = path.join(__dirname, 'pages', 'about.html');
+  res.sendFile(file);
+})
+
+app.get('/contact', (req, res) => {
+  const file = path.join(__dirname, 'pages', 'contact.html');
+  res.sendFile(file);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
+})
+
+app.get('*', (req,res) => {
+  res.status(404).sendFile(path.join(__dirname, 'pages', '404.html'))
 })
