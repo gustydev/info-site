478c641e1c67c04f8898f8e4435ed426abc8a50d
Gustavo Moreira <96840382+gustydev@users.noreply.github.com>
2024-12-16 19:09:52 -0300

    Use HTTPS certificate

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const port = 3000;
+
+// Load SSL certificate and key
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/gustydev.duckdns.org/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/gustydev.duckdns.org/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/gustydev.duckdns.org/chain.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate, ca: ca };
 
 app.get('/', (req, res) => {
   const file = path.join(__dirname, 'pages', 'index.html');
@@ -18,9 +27,9 @@ app.get('/contact', (req, res) => {
   res.sendFile(file);
 })
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+https.createServer(credentials, app).listen(port, () => {
+  console.log(`Server running on https://gustydev.duckdns.org`);
+});
 
 app.get('*', (req,res) => {
   res.status(404).sendFile(path.join(__dirname, 'pages', '404.html'))
